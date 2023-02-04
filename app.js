89d1c1287d0df32331951bfd4f35ad69c3502bcd
Sinixon <joeyburgt@hotmail.com>
2023-02-04 13:53:01 +0100

    Add javascript to make a first working version. There is still a bug when a user enters an incorrect value. The computer will always will in that case.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Welcome');
+console.log('Welcome to a game of rock paper scissors');
+
+const playerSelection = prompt("Rock / Paper / Scissors").toLowerCase();
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a draw. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+    } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+    } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+    } else {
+        return `Computer wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+    }
+}
+
+function getComputerChoice() {
+    const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
+}
+
