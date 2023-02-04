0280497e72f83504205ca28c257f28dee8dc8f2d
Sinixon <joeyburgt@hotmail.com>
2023-02-04 13:57:53 +0100

    Add logic to keep asking for user input if the value does not match either rock paper or scissors. This is the first version of the game.

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
+const ROCK_PAPER_SCISSORS = ['rock', 'paper', 'scissors'];
+let askForInput = true;
+let playerSelection;
+
 console.log('Welcome to a game of rock paper scissors');
 
-const playerSelection = prompt("Rock / Paper / Scissors").toLowerCase();
+while (askForInput) {
+   playerSelection = prompt("rock / paper / scissors").toLowerCase();
+   for (let i = 0; i < ROCK_PAPER_SCISSORS.length; i++) {
+       if (playerSelection === ROCK_PAPER_SCISSORS[i]) {
+           askForInput = false;
+       }
+   }
+}
 
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a draw. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
-    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
-        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
-    } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
-        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
-    } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
-        return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
-    } else {
-        return `Computer wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
-    }
+   if (playerSelection === computerSelection) {
+       return `It's a draw. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+   } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+       return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+   } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+       return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+   } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+       return `Player wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+   } else {
+       return `Computer wins. Player chose: ${playerSelection} and the computer chose: ${computerSelection}`;
+   }
 }
 
 function getComputerChoice() {
-    const pick = ['rock', 'paper', 'scissors'];
-    return pick[Math.floor(Math.random() * pick.length)];
-}
 
+
+   return ROCK_PAPER_SCISSORS[Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)];
+}
