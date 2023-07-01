9c23dc3e19d5dddb9aba0f9cfcd764b129639ceb
Sinixon <joeyburgt@hotmail.com>
2023-07-01 18:04:01 +0200

    Add first to five wins will win. Add a end of 5 wins announcement and logic for this and css for the announcement.

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
-const ROCK_PAPER_SCISSORS = ['rock', 'paper', 'scissors'];
+const ROCK_PAPER_SCISSORS = ['Rock', 'Paper', 'Scissors'];
 let playerSelection;
 let playerWins = 0;
 let computerWins = 0;
 
-//console.log('Welcome to a game of rock paper scissors. You will be playing 5 games in total.');
-
-
 function playRound(playerSelection, computerSelection) {
+    if ((playerWins === 5) || (computerWins === 5)) {
+        resetScoresAndText();
+    }
+
+    if ((playerWins === 0) || (computerWins === 0)) {
+        document.getElementById("endOfGame").innerHTML = "";
+    }
+
     let playerWinsText = `Player wins.<br><br> Player chose: ${playerSelection}<br>Computer chose: ${computerSelection}`;
 
     if (playerSelection === computerSelection) {
         document.getElementById("result").innerHTML = `It's a draw.<br><br>Player chose: ${playerSelection}<br>Computer chose: ${computerSelection}`;
-    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+    } else if ((playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
         playerWins++;
         document.getElementById("result").innerHTML = playerWinsText;
-    } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+    } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock')) {
         playerWins++;
         document.getElementById("result").innerHTML = playerWinsText;
-    } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+    } else if ((playerSelection === 'Scissors') && (computerSelection === 'Paper')) {
         playerWins++;
-        document.getElementById("result").innerHTML = playerWinsText;              
+        document.getElementById("result").innerHTML = playerWinsText;
     } else {
         computerWins++;
-        document.getElementById("result").innerHTML = `Computer wins.<br><br>Player chose: ${playerSelection} <br>Computer chose: ${computerSelection}`;              
+        document.getElementById("result").innerHTML = `Computer wins.<br><br>Player chose: ${playerSelection} <br>Computer chose: ${computerSelection}`;
     }
 
     document.getElementById("playerCounter").innerHTML = playerWins;
-    document.getElementById("computerCounter").innerHTML = computerWins;    
-    
+    document.getElementById("computerCounter").innerHTML = computerWins;
+
+    checkGameState();
+
 }
 
 function getComputerChoice() {
     return ROCK_PAPER_SCISSORS[Math.floor(Math.random() * ROCK_PAPER_SCISSORS.length)];
 }
 
-function game() {
-
-    for (let i = 0; i < ROCK_PAPER_SCISSORS.length; i++) {
-        if (playerSelection === ROCK_PAPER_SCISSORS[i]) {
-            askForInput = false;
-        }
-
-        const computerSelection = getComputerChoice();
-
-
-        console.log(playRound(playerSelection, computerSelection, i));
-        console.log(`Score table: Player: ${playerWins} || Computer: ${computerWins}`);
+function checkGameState() {
+    if (playerWins === 5) {
+        document.getElementById("endOfGame").innerHTML = "Congratulations, you won! Click a button to restart.";
     }
+
+    if (computerWins === 5) {
+        document.getElementById("endOfGame").innerHTML = "Sorry, you lost. Click a button to restart.";
+    }
+
 }
 
-function printWinner() {
-    console.log(`Final score table: Player: ${playerWins} || Computer: ${computerWins}`);
-    if (playerWins > computerWins) {
-        return console.log('The player won');
-    } else if (playerWins < computerWins) {
-        return console.log('The computer won');
-    } else {
-        return console.log('No winner could be determined due to a draw.');
-    }
+function resetScoresAndText() {
+    const playerScoreText = document.querySelector("#playerCounter");
+    const computerScoreText = document.querySelector("#computerCounter");
+
+    playerWins = 0;
+    computerWins = 0;
+
+    playerScoreText.textContent = playerWins;
+    computerScoreText.textContent = computerWins;
 }
